1d6ffdeb0bf88b377e80892df8e67d55260165de
Mike Tintes <michael.tintes@hotmail.com>
2017-05-05 10:27:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 var express = require('express');
 
 var app = express();
-var port = process.env.port || 8080;
 
 app.get('/', function (req, res){
 		res.setHeader('Access-Control-Allow-Origin', 'http://mtintes.com');
 		res.json("Service is up.");
 });
 
-app.listen(port, function(){
-
-});
+app.listen(process.env.PORT || 5000);
