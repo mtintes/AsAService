b662a1da1c69174f0e846f4dc81fb91adb996858
Mike Tintes <michael.tintes@hotmail.com>
2017-05-04 21:19:49 -0500

    initial commit

---
+++ test.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res){
+		res.setHeader('Access-Control-Allow-Origin', 'http://mtintes.com');
+		res.json("Service is up.");
+});
+
+var server = app.listen(3000, function(){
+
+});
