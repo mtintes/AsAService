0b16ec4fe45a2647030e73f4e4e850e3dc3c1331
mtintes <michael.tintes@hotmail.com>
2017-05-21 19:29:00 -0500

    update record every day so no repeats and mark as used.

--- src/server.js
+++ src/server.js
@@ -9,6 +9,8 @@ var response = "";
 
 var mongoUrl = process.env.MONGODB_URI;
 
+var current = {date: "", video:""};
+
 app.get('/videos', function (req, res){
 res.setHeader('Access-Control-Allow-Origin', 'http://mtintes.com');
 	MongoClient.connect(mongoUrl, function(err, db){
@@ -28,18 +30,50 @@ res.setHeader('Access-Control-Allow-Origin', 'http://mtintes.com');
 
 app.get('/', function (req, res){
 res.setHeader('Access-Control-Allow-Origin', 'http://mtintes.com');
+
+console.log("current date: "+current.date);
+
+if(current.date === "" || current.date !== new Date().setHours(0,0,0,0)){
+	console.log("pull new");
 	MongoClient.connect(mongoUrl, function(err, db){
 
 		var col = db.collection('good_morning');
 
-		col.find().toArray(
+		if(current.video){
+			console.log("video is "+current.video);
+			col.findOne({ "video": current.video }, function(err,result){
+				console.log(result);
+				console.log(result.used);
+				console.log(result.video);
+				console.log(result._id);
+
+				console.log("updating yesterdays video");
+				col.update({_id:result._id}, {$set: {used:true}});
+			});
+		}
+
+		col.find({"used":{ "$ne": true}}).toArray(
 			 function(err, result){
-				res.json("https://youtube.com/watch?v="+result[0].video);
+				 console.log("setting video");
+				 console.log(result[0].video);
+				 console.log(result[0]._id);
+				current.video = result[0].video;
+				current.date = new Date().setHours(0,0,0,0);
+				console.log("new date: " + current.date);
+				res.json("https://youtube.com/watch?v="+current.video);
 			db.close();
 		});
 	});
 
+}else{
+		console.log("video already exists");
+		console.log(current.video);
+		res.json("https://youtube.com/watch?v="+current.video);
+}
 });
+
+
+
 
 app.post('/add', function (req,res){
 	console.log(req.body.video);
